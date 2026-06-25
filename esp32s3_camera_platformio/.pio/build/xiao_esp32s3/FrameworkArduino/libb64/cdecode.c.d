b829409c41261ae1ec6eb6b77053c2ba7727e1b9
ufukushima-a11y <u.fukushima@kimura-lab.net>
2026-06-25 16:35:29 +0900

    a

---
+++ esp32s3_camera_platformio/.pio/build/xiao_esp32s3/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/xiao_esp32s3/FrameworkArduino/libb64/cdecode.c.o: \
+ C:/Users/ufuku/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ C:/Users/ufuku/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
---
+++ esp32s3_camera_platformio/.pio/build/xiao_esp32s3/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/xiao_esp32s3/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/ufuku/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/ufuku/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
