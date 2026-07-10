6a3324d0908768eb375042e5dc23b5601b5e4f77
higuchi <higuchi@kimura-lab.net>
2026-07-10 12:06:13 +0900

    a

---
+++ tlm922s_p2p/esp32c3_tlm922s_diagnostic/.pio/build/seeed_xiao_esp32c3/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/seeed_xiao_esp32c3/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/higuc/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/higuc/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/higuc/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
