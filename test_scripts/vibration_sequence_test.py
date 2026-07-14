fadcc2b88a1c5b97f92bcd0c2a308b81892336b9
Hiromichi <h.ito@kimura-lab.net>
2026-07-14 16:21:07 +0900

    a

--- test_scripts/vibration_sequence_test.py
+++ test_scripts/vibration_sequence_test.py
@@ -158,6 +158,9 @@ def main() -> None:
             driver.flip()
             driver.reverse_stabilizer()
 
+            # 姿勢の正常化
+            NavigationController().restore_posture(driver, sensors)
+
             # 4. 前方カメラの赤色検知結果に応じてパラシュートを回避する。
             avoidance_result = NavigationController().avoid_parachute(driver, sensors)
         finally:
