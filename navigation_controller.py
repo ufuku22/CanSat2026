fe7f27496c779f483bdb503c0a79e052d54403c5
Hiromichi <h.ito@kimura-lab.net>
2026-07-14 17:11:56 +0900

    a

--- navigation_controller.py
+++ navigation_controller.py
@@ -106,9 +106,7 @@ class NavigationController:
         
         accel_y = float(sensor_manager.get_imu()["accel_mps2"][1])
         if accel_y <= -7.0:
-            driver.drive(60)
-            time.sleep(0.3)
-            driver.brake()
+            driver.reverse_stabilizer(speed=50)
             time.sleep(3.0)
 
         accel_z = float(sensor_manager.get_imu()["accel_mps2"][2])
