905ac1e6c8ab79d34097927f99cb027ecf54ebaa
Hiromichi <h.ito@kimura-lab.net>
2026-07-25 17:09:47 +0900

    a

--- config.py
+++ config.py
@@ -155,7 +155,7 @@ class RedBallConfig:
     FORWARD_DURATION_BY_DISTANCE_M = (
         (2.0, 0.48),
         (1.5, 0.36),
-        (1.0, 0.24),a
+        (1.0, 0.24),
         (0.8, 0.12),
     )
 
