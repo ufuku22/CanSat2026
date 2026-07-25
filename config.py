62653a2e4a63bfe163edde82babffe6e4cb98fb4
Hiromichi <h.ito@kimura-lab.net>
2026-07-25 17:00:55 +0900

    a

--- config.py
+++ config.py
@@ -146,11 +146,14 @@ class RedBallConfig:
     TARGET_DISTANCE_M = 0.8
     MAX_DISTANCE_APPROACH_STEPS = 10
     CONE_FORWARD_DURATION_BY_RED_RATIO = (
-        (0.005, 0.05),
-        (0.001, 0.10),
+        (0.005, 0.15),
+        (0.003, 0.20),
     )
     FORWARD_DURATION_S = 0.10
     FORWARD_DURATION_BY_DISTANCE_M = (
+        (2.0, 0.12),
+        (1.5, 0.09),
+        (1.0, 0.06),
         (0.8, 0.03),
     )
 
