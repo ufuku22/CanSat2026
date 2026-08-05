a30602176c758bfb9d134395c7fbaa71ad47876e
higuchi <higuchi@kimura-lab.net>
2026-08-05 13:25:05 +0900

    a

--- mission_arliss.py
+++ mission_arliss.py
@@ -7,8 +7,8 @@ from mission_controller import MissionController
 from navigation_controller import NavigationController
 import time
 
-TARGET_LATITUDE_DEG = 35.0    # 目標緯度
-TARGET_LONGITUDE_DEG = 139.0  # 目標経度
+TARGET_LATITUDE_DEG = 35.9230760    # 目標緯度
+TARGET_LONGITUDE_DEG = 139.9102107  # 目標経度
 
 
 def main() -> None:
