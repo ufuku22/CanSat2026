e29e8b5a40b22fa6798e370291706f6f452d3dea
sakanamochi <siomichi@outlook.jp>
2026-09-15 04:17:41 +0900

    a

--- test_scripts/gps_pd_navigation_test.py
+++ test_scripts/gps_pd_navigation_test.py
@@ -14,8 +14,8 @@ from drive_controller import DriveController
 from navigation_controller import NavigationController
 from sensor_manager import SensorManager
 
-TARGET_LATITUDE_DEG = 35.9188814    # 目標緯度
-TARGET_LONGITUDE_DEG = 139.9093615  # 目標経度
+TARGET_LATITUDE_DEG = 40.874125283    # 目標緯度
+TARGET_LONGITUDE_DEG = -119.154997983 # 目標経度
 
 
 def setup_navigation_sensors(sensors: SensorManager) -> None:
